8a46cc2558d8c17bbe375f0760813e7ed3e8c492
Tanish Khare <tanishkhare@gmail.com>
2020-03-22 01:58:06 +0530

    Added color 16 and color 17

--- config.h
+++ config.h
@@ -183,6 +183,8 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
+		{ "color16",      STRING,  &colorname[16] },
+		{ "color17",      STRING,  &colorname[177] },
 		{ "background",   STRING,  &colorname[256] },
 		{ "foreground",   STRING,  &colorname[257] },
 		{ "cursorColor",  STRING,  &colorname[258] },

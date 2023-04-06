27b1e1e75a6154544c2de9f8c0038be5d73a41e6
Tanish2002 <tanishkhare@gmail.com>
2023-04-06 17:39:24 +0530

    add devshell

--- flake.nix
+++ flake.nix
@@ -2,12 +2,20 @@
   description = "A flake for installing my st build";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
 
-  outputs = { self, nixpkgs, ... }: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      pkgs.st.overrideAttrs (o: {
+  outputs = { self, nixpkgs, ... }:
+    with import nixpkgs { system = "x86_64-linux"; };
+    let
+      st-tanish2002 = pkgs.st.overrideAttrs (o: {
         src = self;
         buildInputs = o.buildInputs ++ [ pkgs.harfbuzz ];
       });
-  };
+    in {
+      defaultPackage.x86_64-linux = st-tanish2002;
+      # tools for development
+      devShell.x86_64-linux = pkgs.mkShell {
+        packages = [ pkgs.clang-tools ] ++ st-tanish2002.buildInputs
+          ++ st-tanish2002.nativeBuildInputs;
+      };
+
+    };
 }
